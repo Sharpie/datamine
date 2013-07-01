ece2ddbf3864ff892f85aeaeea34b138331394e3
Charlie Sharpsteen <source@sharpsteen.net>
2013-07-01 01:10:30 -0700

    (WIP) Extend the redmine command to fetch CSV
    It is a steaming pile right now, but it works.

--- lib/datamine/cli/redmine.rb
+++ lib/datamine/cli/redmine.rb
@@ -1,15 +1,55 @@
-require 'pry'
+require 'csv'
 require 'datamine/redmine/rest'
+require 'datamine/redmine/util'
 
 module Datamine::CLI
   desc 'Fetch data from redmine'
   arg_name 'URL'
   command :redmine do |c|
     c.action do |global_options,options,args|
-      Issue = Datamine::Redmine::REST.factory args.first
-      issues = Issue.find(:all)
+      # FIXME this is a hot mess.
+      log_file = File.open '/tmp/issues.json', 'w' # So we have a checkpoint to recover
+      log_file.write '['
+      flag = false
 
-      puts issues.to_json
+      records = []
+      attributes = Set.new
+
+      Issues = Datamine::Redmine::REST.factory args.first
+
+      # FIXME These need to be options!
+      offset      = 0
+      limit       = 100
+      total_count = Issues.find(:all, :params => {:limit => 1}).total_count
+      params      = {}
+
+      Datamine::Redmine::REST.paginate(Issues, offset, limit, total_count, params) do |page|
+        records += page.map do |issue|
+          flag ? log_file.write(',') : (flag = true)
+          log_file.write issue.to_json
+          log_file.flush
+
+          r = Datamine::Redmine::Util.flatten_issue issue
+          attributes.merge r.keys
+          r
+        end
+      end
+
+      log_file.write ']'
+      log_file.close
+
+      $stderr.puts 'Raw data saved to /tmp/issues.json'
+
+      CSV.open '/tmp/issues.csv', 'wb' do |csv|
+        csv << attributes
+        records.map do |record|
+          csv << attributes.map {|k| record[k]}
+        end
+      end
+
+      $stderr.puts 'Issues saved to /tmp/issues.csv'
+
+      true
     end
   end
 end
